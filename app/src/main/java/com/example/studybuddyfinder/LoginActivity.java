eeeeee46a26dd7e2ebcabe346431097f190fa855
dzsnowings <dzsnowings@gmail.com>
2019-11-16 17:18:40 -0500

    Fix activity_main button IDs

--- app/src/main/java/com/example/studybuddyfinder/LoginActivity.java
+++ app/src/main/java/com/example/studybuddyfinder/LoginActivity.java
@@ -1,5 +1,20 @@
 package com.example.studybuddyfinder;
 
-public class LoginActivity {
+import android.os.Bundle;
+import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_login);
+
+        EditText usernameText = findViewById(R.id.username);
+        EditText passwordText = findViewById(R.id.password);
+
+        String username = usernameText.getText().toString();
+        String password = passwordText.getText().toString();
+    }
 }
--- app/src/main/java/com/example/studybuddyfinder/SignupActivity.java
+++ app/src/main/java/com/example/studybuddyfinder/SignupActivity.java
@@ -1,5 +1,45 @@
 package com.example.studybuddyfinder;
 
-public class SignupActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_signup);
+
+        EditText firstNameText = findViewById(R.id.firstName);
+        EditText lastNameText = findViewById(R.id.lastName);
+        EditText emailText = findViewById(R.id.email);
+        EditText phoneText = findViewById(R.id.phone);
+        EditText usernameText = findViewById(R.id.username);
+        EditText passwordText = findViewById(R.id.password);
+        EditText confirmText = findViewById(R.id.confirmPassword);
+        Button signupButton = findViewById(R.id.signupButton);
+
+        String firstName = firstNameText.getText().toString();
+        String lastName = lastNameText.getText().toString();
+        String email = emailText.getText().toString();
+        String phone = phoneText.getText().toString();
+        String username = usernameText.getText().toString();
+        final String password = passwordText.getText().toString();
+        final String confirm = confirmText.getText().toString();
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!password.equals(confirm)) {
+                    String error = "Passwords do not match.";
+                    Toast.makeText(SignupActivity.this, error, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 }
