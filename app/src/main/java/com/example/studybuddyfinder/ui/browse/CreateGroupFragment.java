d991f37d82935c360fe5f7377466c108ef3f5950
Azure-Skies <Azure-Skies@users.noreply.github.com>
2019-11-17 01:45:23 -0500

    i did absolutely nothing

--- app/src/main/java/com/example/studybuddyfinder/ui/browse/CreateGroupFragment.java
+++ app/src/main/java/com/example/studybuddyfinder/ui/browse/CreateGroupFragment.java
@@ -4,11 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.studybuddyfinder.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateGroupFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -18,6 +25,37 @@ public class CreateGroupFragment extends Fragment {
     }
 
     public void onViewCreated(View view, Bundle savedInstantState) {
+        List<String> SpinnerArray = new ArrayList<String>();
+        SpinnerArray.add("AM");
+        SpinnerArray.add("PM");
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, SpinnerArray);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner spinner = (Spinner) findViewById(R.id.timeSpinner);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view,
+                                       int position, long id) {
+                Object item = adapterView.getItemAtPosition(position);
+                if (item != null) {
+                    Toast.makeText(CreateGroupFragment.this, item.toString(),
+                            Toast.LENGTH_SHORT).show();
+                }
+                Toast.makeText(CreateGroupFragment.this, "Selected",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // TODO Auto-generated method stub
+
+            }
+        });
     }
 }
