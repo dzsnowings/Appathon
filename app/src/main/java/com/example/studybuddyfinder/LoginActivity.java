09f0466902758fc9f257b6128e159b191df6069d
mpurushothapu3 <55211137+mpurushothapu3@users.noreply.github.com>
2019-11-17 03:33:18 -0500

    Added Database Integration to login and signup

--- app/src/main/java/com/example/studybuddyfinder/LoginActivity.java
+++ app/src/main/java/com/example/studybuddyfinder/LoginActivity.java
@@ -2,12 +2,22 @@ package com.example.studybuddyfinder;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
@@ -28,14 +38,35 @@ public class LoginActivity extends AppCompatActivity {
                 if (!password.equals(password)) {
                     String message = "Username and/or password is incorrect. Please try again.";
                     Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
-                } else if (!(password.equals("") && username.equals(""))) {
+                } else if (password.equals("") || username.equals("")) {
                     String message = "You did not enter username or password";
                     Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
                 } else {
-                    String message = "Successfully logged in.";
-                    Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(LoginActivity.this, ContentActivity.class);
-                    startActivity(intent);
+                    FirebaseFirestore.getInstance().collectionGroup("Users").whereEqualTo("Username", "dzhao").whereEqualTo("Password", password)
+                            .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                Log.d("GG", "Started");
+                                int count = 0;
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    count++;
+                                    Log.d("GG", document.getId() + " => " + document.getData());
+                                    Intent intent = new Intent(LoginActivity.this, ContentActivity.class);
+                                    startActivity(intent);
+                                }
+                                if(count == 0) {
+                                Log.d("GG", "REJECTED");
+                                String message = "You have not signed up yet.";
+                                Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show(); }
+                            } else {
+                                Log.d("GG", "Error getting documents: ", task.getException());
+                                String message = "There was an error connecting.";
+                                Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
                 }
             }
         });
--- app/src/main/java/com/example/studybuddyfinder/SignupActivity.java
+++ app/src/main/java/com/example/studybuddyfinder/SignupActivity.java
@@ -2,18 +2,30 @@ package com.example.studybuddyfinder;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class SignupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
 
         final EditText firstNameText = findViewById(R.id.firstName);
         final EditText lastNameText = findViewById(R.id.lastName);
@@ -39,16 +51,45 @@ public class SignupActivity extends AppCompatActivity {
                     String error = "Passwords do not match.";
                     Toast.makeText(SignupActivity.this, error, Toast.LENGTH_SHORT).show();
                 }
-
                 if (!password.equals(confirm) || firstName.equals("") || lastName.equals("") || email.equals("") || username.equals("") || password.equals("")) {
                     String error = "Some required information is missing.";
                     Toast.makeText(SignupActivity.this, error, Toast.LENGTH_SHORT).show();
                 }
                 else {
-                        Intent intent = new Intent(SignupActivity.this, ContentActivity.class);
-                        startActivity(intent);
-                    }
+
+                    Map<String, String> user = new HashMap<>();
+                    user.put("First", firstName);
+                    user.put("Last", lastName);
+                    user.put("Email", email);
+                    user.put("Phone", phone);
+                    user.put("Username", username);
+                    user.put("Password", password);
+
+                    final String key = Integer.toHexString(username.hashCode() + password.hashCode());
+                    // Add a new document with a generated ID
+                    FirebaseFirestore.getInstance().collection("Users")
+                            .document(key)
+                            .set(user)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d("GG", "DocumentSnapshot added with ID: " + key);
+                                    Intent intent = new Intent(SignupActivity.this, ContentActivity.class);
+                                    startActivity(intent);
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w("GG", "Error adding document", e);
+                                    String error = "There was an error signing you up.";
+                                    Toast.makeText(SignupActivity.this, error, Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
                 }
-            });
-        }
+            }
+        });
     }
+}
+
